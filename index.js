17033142a263ede02eaf3f23ba916bb4f4985cb3
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-04 19:23:24 +0300

    new commit on git project

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ console.log(subjects.length);
 subjects[1] = "geo";
 
 console.log(subjects);
+
+
+
+
+function arrayListing(array) {
+    return console.log(subjects[0].toUpperCase())
+} 
+
+arrayListing();
+
+
+
+
+
+
+const superheroes = ["Catwoman", "She-Hulk", "Jessica Jones"];
+
+superheroes.push("Wonder Woman");
+// => 4
+
+console.log(superheroes);
+// => ["Catwoman", "She-Hulk", "Jessica Jones", "Wonder Woman"]
+
+const cities = ["New York", "San Francisco"];
+
+cities.unshift("bostoni", "Chicago");
+// => 4
+
+console.log(cities);
+
+
+const cities1 = [...cities];
+
+
+console.log(cities1);
+console.log(cities);
+
+
+
+
